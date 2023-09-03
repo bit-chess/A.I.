f0342d74d56b96ef783ebb503787cb8c18577bc4
SageScroll18144 <ffss@cin.ufpe.br>
2023-09-03 08:04:21 -0300

    realizando um teste

---
+++ src/tests/arg_test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print(char a[8][8]){
+    for(int i=0;i<8;i++) for(int j=0;j<8;j++) printf("%c", a[i][j]);
+}
+
+int main(){
+    char ans[8][8];
+    for(int i=0;i<8;i++) for(int j=0;j<8;j++) ans[i][j] = 'x';
+    print(ans);
+
+    return 0;
+}
