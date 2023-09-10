32089d40a08df66e114297c87d86a8622214506d
Pedro Barros <52806693+lightTuring@users.noreply.github.com>
2023-09-10 13:27:37 -0300

    metodo para criar movimento até uma peça

--- src/Rules/manipulator.cpp
+++ src/Rules/manipulator.cpp
@@ -13,3 +13,12 @@ int lsb(int64_t bit){
     std::bitset<64> bits(bit);
     return bits._Find_fist();
 }
+
+int64_t northFillBlock(int64_t b, int64_t emp){
+    b |= emp & (g <<  8);
+    emp &=     (p <<  8);
+    b |= emp & (g << 16);
+    emp &=     (p << 16);
+    b |= emp & (g << 32);
+    return b;
+}
